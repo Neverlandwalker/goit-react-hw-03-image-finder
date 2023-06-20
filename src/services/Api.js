2bd14a658cffe4cfed670412a85589dc7bf43211
Neverlandwalker <Blonnyya@gmail.com>
2023-06-20 20:29:03 +0300

    last one

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,16 +3,9 @@ import axios from 'axios';
 export const fetchImage = async (query, page) => {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '30249749-d55f47b4585d1b9efabc8a3ca';
-  if (query === '') {
-    return
-  } else {
-    const response = await axios.get(
-      `${BASE_URL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    )
-
-    return response.data
-  }
-    
-  
+  const response = await axios.get(
+    `${BASE_URL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  )
+  return response.data
 };
 
